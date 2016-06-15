5102b0ea366bcde0ad5ac75772f81ef19c069a47
Vicheanak <vicheanak@gmail.com>
2016-06-15 16:35:41 +0700

    first commit

---
+++ app/app.js
@@ -0,0 +1,50 @@
+import {Component, ViewChild} from '@angular/core';
+import {ionicBootstrap, Platform, MenuController} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
+
+
+@Component({
+  templateUrl: 'build/app.html',
+  queries: {
+    nav: new ViewChild('content')
+  }
+})
+class MyApp {
+  static get parameters() {
+    return [[Platform], [MenuController]];
+  }
+
+  constructor(platform, menu) {
+    this.platform = platform;
+    this.menu = menu;
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'Hello Ionic', component: HelloIonicPage },
+      { title: 'My First List', component: ListPage }
+    ];
+
+    // make HelloIonicPage the root (or first) page
+    this.rootPage = HelloIonicPage;
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
+
+ionicBootstrap(MyApp);
---
+++ app/pages/item-details/item-details.js
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+import {NavController, NavParams} from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'build/pages/item-details/item-details.html'
+})
+export class ItemDetailsPage {
+  static get parameters() {
+    return [[NavController], [NavParams]];
+  }
+
+  constructor(nav, navParams) {
+    this.nav = nav;
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+  }
+}
